bff2fda05e4b782804408809d96451db5bfd0735
Blue Staggo <owengrenney@gmail.com>
2024-07-25 20:57:40 +0100

    Optional Zombie Pigman flesh drops

---
+++ client/src/main/java/io/bluestaggo/tweakedadventure/mixin/ZombiePigmanEntityMixin.java
@@ -0,0 +1,28 @@
+package io.bluestaggo.tweakedadventure.mixin;
+
+import io.bluestaggo.tweakedadventure.TweakedAdventureConfig;
+import net.minecraft.entity.living.mob.hostile.ZombieEntity;
+import net.minecraft.entity.living.mob.hostile.ZombiePigmanEntity;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ZombiePigmanEntity.class)
+public abstract class ZombiePigmanEntityMixin extends ZombieEntity {
+	private ZombiePigmanEntityMixin(World world) {
+		super(world);
+	}
+
+	@Redirect(
+		method = "getDroppedItem",
+		at = @At(
+			value = "FIELD",
+			target = "Lnet/minecraft/item/Item;COOKED_PORKCHOP:Lnet/minecraft/item/Item;"
+		)
+	)
+	private Item getDroppedFlesh() {
+		return TweakedAdventureConfig.getInstance().pigmenDropPorkchops() ? Item.ROTTEN_FLESH : Item.COOKED_PORKCHOP;
+	}
+}
