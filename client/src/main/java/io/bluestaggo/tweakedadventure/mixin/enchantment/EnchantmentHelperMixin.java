d51358848bd98d980599d7bbe1a77163ee56603e
Blue Staggo <owengrenney@gmail.com>
2025-07-21 14:43:03 +0100

    Make xp setting work for enchantments

--- client/src/main/java/io/bluestaggo/tweakedadventure/mixin/enchantment/EnchantmentHelperMixin.java
+++ client/src/main/java/io/bluestaggo/tweakedadventure/mixin/enchantment/EnchantmentHelperMixin.java
@@ -1,12 +1,14 @@
 package io.bluestaggo.tweakedadventure.mixin.enchantment;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import io.bluestaggo.tweakedadventure.TweakedAdventureConfig;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -14,35 +16,43 @@ import java.util.*;
 
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
-	/**
-	 * @author BlueStaggo
-	 * @reason 1.3 enchantment mechanics
-	 */
-	@Overwrite
-	public static int getRequiredXpLevel(Random random, int entry, int max, ItemStack stack) {
+	@Inject(
+		method = "getRequiredXpLevel",
+		at = @At("HEAD"),
+		cancellable = true
+	)
+	private static void getRequiredXpLevel(Random random, int entry, int max, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+		if (!TweakedAdventureConfig.getInstance().lowerXpRequirement()) {
+			return;
+		}
+
 		Item var4 = stack.getItem();
 		int var5 = var4.getEnchantability();
 		if(var5 <= 0) {
-			return 0;
+			cir.setReturnValue(0);
 		} else {
 			if(max > 15) {
 				max = 15;
 			}
 
 			int var6 = random.nextInt(8) + 1 + (max >> 1) + random.nextInt(max + 1);
-			return entry == 0 ? Math.max(var6 / 3, 1) : (entry == 1 ? var6 * 2 / 3 + 1 : Math.max(var6, max * 2));
+			cir.setReturnValue(entry == 0 ? Math.max(var6 / 3, 1) : (entry == 1 ? var6 * 2 / 3 + 1 : Math.max(var6, max * 2)));
 		}
 	}
 
-	@Redirect(
+	@WrapOperation(
 		method = "getEnchantmentEntries",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/item/Item;getEnchantability()I"
 		)
 	)
-	private static int getR13EnchantmentEntries1(Item item) {
-		return item.getEnchantability() / 2;
+	private static int getR13EnchantmentEntries1(Item instance, Operation<Integer> original) {
+		int enchantability = original.call(instance);
+		if (TweakedAdventureConfig.getInstance().lowerXpRequirement()) {
+			enchantability /= 2;
+		}
+		return enchantability;
 	}
 
 	@Inject(
@@ -53,10 +63,10 @@ public abstract class EnchantmentHelperMixin {
 			shift = At.Shift.BEFORE
 		)
 	)
-	private static void getR13EnchantmentEntries1(Random random, ItemStack stack, int xpLevel,
+	private static void getR13EnchantmentEntries2(Random random, ItemStack stack, int xpLevel,
 												  CallbackInfoReturnable<List> cir,
 												  @Local(ordinal = 3) LocalIntRef var7) {
-		if (var7.get() < 1) {
+		if (TweakedAdventureConfig.getInstance().lowerXpRequirement() && var7.get() < 1) {
 			var7.set(1);
 		}
 	}
--- client/src/main/java/io/bluestaggo/tweakedadventure/mixin/enchantment/SilkTouchEnchantmentMixin.java
+++ client/src/main/java/io/bluestaggo/tweakedadventure/mixin/enchantment/SilkTouchEnchantmentMixin.java
@@ -1,9 +1,12 @@
 package io.bluestaggo.tweakedadventure.mixin.enchantment;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import io.bluestaggo.tweakedadventure.TweakedAdventureConfig;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentCategory;
 import net.minecraft.enchantment.SilkTouchEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
@@ -13,13 +16,17 @@ public abstract class SilkTouchEnchantmentMixin extends Enchantment {
 		super(id, type, category);
 	}
 
-	@ModifyConstant(
+	@ModifyExpressionValue(
 		method = "getMinXpRequirement",
-		constant = @Constant(
-			intValue = 25
+		at = @At(
+			value = "CONSTANT",
+			args = "intValue=25"
 		)
 	)
-	private int getMinXpRequirementR13(int constant) {
+	private int getMinXpRequirementR13(int original) {
+		if (!TweakedAdventureConfig.getInstance().lowerXpRequirement()) {
+			return original;
+		}
 		return 15;
 	}
 }
